009bfadaf6438592719f37095c10a501efa4582a
WailAmbs <wail.m@ambs-agency.com>
2025-03-27 12:16:58 +0100

    Change pass hash bycript

--- change-password.php
+++ change-password.php
@@ -8,26 +8,28 @@ if(isset($_SESSION['login']))    {
     else{
 if(isset($_POST['submit']))
     {
-$password=md5($_POST['password']);
-$newpassword=md5($_POST['newpassword']);
-$username=$_SESSION['alogin'];
-    $sql ="SELECT Password FROM users WHERE UserName=:username and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
-if($query -> rowCount() > 0)
+$password = $_POST['password'];
+$newpassword = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
+$username = $_SESSION['alogin'];
+
+// Récupérer le mot de passe haché actuel
+$sql = "SELECT Password FROM users WHERE UserName=:username";
+$query = $dbh->prepare($sql);
+$query->bindParam(':username', $username, PDO::PARAM_STR);
+$query->execute();
+$result = $query->fetch(PDO::FETCH_ASSOC);
+
+if($result && password_verify($password, $result['Password']))
 {
-$con="update users set Password=:newpassword where UserName=:username";
-$chngpwd1 = $dbh->prepare($con);
-$chngpwd1-> bindParam(':username', $username, PDO::PARAM_STR);
-$chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
-$chngpwd1->execute();
-$msg="Your Password succesfully changed";
+    $con = "update users set Password=:newpassword where UserName=:username";
+    $chngpwd1 = $dbh->prepare($con);
+    $chngpwd1->bindParam(':username', $username, PDO::PARAM_STR);
+    $chngpwd1->bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
+    $chngpwd1->execute();
+    $msg = "Your Password succesfully changed";
 }
 else {
-$error="Your current password is wrong";    
+    $error = "Your current password is wrong";    
 }
 }
 ?>
